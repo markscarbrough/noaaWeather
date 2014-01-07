35225d7e858a24457120dbf07feccfcac048334d
Mark Scarbrough <markscarbrough@gmail.com>
2014-01-07 10:03:28 -0500

    Changed cache path to Yii default

--- NoaaCacheBehavior.php
+++ NoaaCacheBehavior.php
@@ -8,30 +8,11 @@
  
  /**
  * Caches downloaded data to improve application performance and reduce load 
- * on the NOAA servers. By default the extension will create a new CFileCache
- * repository in its own cache subdirectory. This path must be writeable by the
- * webserver. The cache path can be changed by adding keys to the application-
- * level parameters array in the main web configuration file 
- * (main.php), for exmample:
- * 
- * // application-level parameters that can be accessed
- * // using Yii::app()->params['paramName']
- * // Change noaaWeather.cachePath from the default below
- * // Set to null to use the application file cache path (protected/runtime/cache)
- * 'params'=>array(
- * 	'noaaWeather.cachePath' => 'protected/extensions/noaaWeather/cache'
- * );
- *  
+ * on the NOAA servers. By default the extension will use the CFileCache
+ * repository under protected/runtime/cache. This path must be writeable by the
+ * webserver. 
  */
 class NoaaCacheBehavior extends CBehavior {
-
-	/**
-	* @var string path to store cache files
-	* Do not change this file, instead add the 'noaaWeather.cachePath' key to the
-	* application-level parameters array in Yii's main web application configuration
-	* file (main.php)
-	*/
-	public $cachePath='protected/extensions/noaaWeather/cache';
 
 	/**
 	* @var object the CFileCache object
@@ -53,11 +34,7 @@ class NoaaCacheBehavior extends CBehavior {
 	*/
 	public function getCacheObject(){
 		if(!is_object($this->cacheObject)){
-			if(isset(Yii::app()->params['noaaWeather.cachePath'])){
-				$this->cachePath = Yii::app()->params['noaaWeather.cachePath'];
-			}
 			$this->cacheObject=new CFileCache();
-			$this->cacheObject->cachePath = $this->cachePath;
 			$this->cacheObject->init();
 		}
 		return $this->cacheObject;
